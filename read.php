38b8b72cf3a85791fae696cb7dc6b29fa7020f06
Thomas Gomree <gomreethomas@gmail.com>
2023-11-14 09:56:31 +0100

    added files

---
+++ read.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Randonnées</title>
+    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <h1>Liste des randonnées</h1>
+    <table>
+      <!-- Afficher la liste des randonnées -->
+    </table>
+  </body>
+</html>
