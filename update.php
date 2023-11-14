d12a85a7ae14d27eca2d80ec9a80e8359050e977
Thomas Gomree <gomreethomas@gmail.com>
2023-11-14 10:56:26 +0100

    Update form : update hiking

--- update.php
+++ update.php
@@ -1,39 +1,75 @@
+<?php
+// Connect to the database
+$db = new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'toms', 'root');
+
+// Get the ID from the URL
+$id = $_GET['id'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// Get the data from the form
+	$name = $_POST['name'];
+	$difficulty = $_POST['difficulty'];
+	$distance = $_POST['distance'];
+	$duration = $_POST['duration'];
+	$height_difference = $_POST['height_difference'];
+
+	// Prepare the SQL statement
+	$stmt = $db->prepare('UPDATE hiking SET name = ?, difficulty = ?, distance = ?, duration = ?, height_difference = ? WHERE id = ?');
+
+	// Execute the statement with the new data
+	$stmt->execute([$name, $difficulty, $distance, $duration, $height_difference, $id]);
+
+	// Redirect to the list page
+	header('Location: /Hiking/read.php');
+	exit;
+}
+
+// Prepare the SQL statement
+$stmt = $db->prepare('SELECT * FROM hiking WHERE id = ?');
+
+// Execute the statement with the ID
+$stmt->execute([$id]);
+
+// Fetch the record
+$row = $stmt->fetch();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Modifier une randonnée</title>
-	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
-	<style>
-		body {
-			background-color: #f0f0f0;
-			font-family: Arial, sans-serif;
-		}
-		form {
-			width: 300px;
-			margin: 0 auto;
-		}
-		label {
-			display: block;
-			margin-top: 20px;
-		}
-		input, select {
-			width: 100%;
-			padding: 10px;
-			margin-top: 5px;
-		}
-		button {
-			width: 100%;
-			padding: 10px;
-			background-color: #007BFF;
-			color: white;
-			border: none;
-			margin-top: 20px;
-		}
-		button:hover {
-			background-color: #0056b3;
-		}
-	</style>
+		<style>
+			body {
+				background-color: #f0f0f0;
+				font-family: Arial, sans-serif;
+			}
+			form {
+				width: 300px;
+				margin: 0 auto;
+			}
+			label {
+				display: block;
+				margin-top: 20px;
+			}
+			input, select {
+				width: 100%;
+				padding: 10px;
+				margin-top: 5px;
+			}
+			button {
+				width: 100%;
+				padding: 10px;
+				background-color: #007BFF;
+				color: white;
+				border: none;
+				margin-top: 20px;
+			}
+			button:hover {
+				background-color: #0056b3;
+			}
+		</style>
+	
 </head>
 <body>
 	<a href="/Hiking/read.php">Liste des données</a>
